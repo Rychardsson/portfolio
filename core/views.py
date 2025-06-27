fa3dd4ed5a4dcee042da3233cac2f67a299be477
Rychardsson <rychardssong2@gmail.com>
2025-06-26 21:12:58 -0300

    Views e rotas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Experiencia, Projeto
 
-# Create your views here.
+def home(request):
+    experiencias = Experiencia.objects.all()
+    projetos = Projeto.objects.all()
+    return render(request, 'core/home.html', {
+        'experiencias': experiencias,
+        'projetos': projetos
+    })
