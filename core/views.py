63a760c47078003940de76cafa99857a7b6afd47
Rychardsson <rychardssong2@gmail.com>
2025-06-28 02:20:19 -0300

    update

--- core/views.py
+++ core/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from .models import Experiencia, Projeto
 
 def home(request):
-    experiencias = Experiencia.objects.all()
-    projetos = Projeto.objects.all()
-    return render(request, 'core/home.html', {
-        'experiencias': experiencias,
-        'projetos': projetos
-    })
+    return render(request, 'core/home.html')
